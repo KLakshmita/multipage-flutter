63d013f0c0b0c465637c0cd4b8652c353c8c443f
KLakshmita <lakshmita13kotari@gmail.com>
2020-04-27 13:16:37 +0530

    mulripage

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/fitness.jpg",
+  "assets/food3.jpg",
+  "assets/fit3.jpeg",
+  "assets/new_fit.jpg",
+];
+
+List<String> title = [
+  "Work hard and play harder",
+  "Healthy food leads to a healthy mind",
+  "Wake up beauty it's time to be beast",
+  "Yoga is Ocean of bliss",
+];
